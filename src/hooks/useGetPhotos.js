5ef634a160556ed1b8d154ebeb65f97d36cf9003
Daniil Simanovich <s1mahochukvas@vk.com>
2022-10-12 09:51:47 +0300

    test

--- src/hooks/useGetPhotos.js
+++ src/hooks/useGetPhotos.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 const useGetPhotos = (requestState, query = null) => {
 	const [state, setState] = useState();
 	const { req, setReq } = requestState;
-
+	// notabug
 	const func = async () => {
 		if (req === 1) return;
 		const res = await fetch(`${process.env.SITE_URL}/api/getPhotos`);
