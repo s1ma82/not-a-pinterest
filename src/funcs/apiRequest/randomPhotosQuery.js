c0049e067f261becb3b6660fe563242c441278c5
Daniil Simanovich <s1mahochukvas@vk.com>
2022-10-13 13:52:35 +0300

    added by funcs folder

---
+++ src/funcs/apiRequest/randomPhotosQuery.js
@@ -0,0 +1,11 @@
+export default async function ({
+	setState: setState,
+	setReq: setReq,
+	req: req,
+}) {
+	if (req === 1) return;
+	const res = await fetch(`${process.env.API_PATHS.randomPhotos}`);
+	const data = (await res.json()).data.response;
+	setState(data);
+	setReq(1);
+}
---
+++ src/funcs/index.js
@@ -0,0 +1,3 @@
+import randomPhotosQuery from "./apiRequest/randomPhotosQuery";
+
+export { randomPhotosQuery };
