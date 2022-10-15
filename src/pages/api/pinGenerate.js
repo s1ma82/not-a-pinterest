1328f5982bca3c3af5356840822c4bd01d540e36
Daniil Simanovich <s1mahochukvas@vk.com>
2022-10-15 11:54:03 +0300

    api modified

--- src/pages/api/pinGenerate.js
+++ src/pages/api/pinGenerate.js
@@ -2,8 +2,14 @@ import {unsplash} from '.'
 
 export default async function handler(req, res) {
 	try {
-		const {pid} = req.query
-		console.log(pid)
-		// await unsplash.photos.get({});
-	} catch {}
+		const id = req.body.id
+		const reqResult = await unsplash.photos.get({ photoId: id })
+		switch (reqResult.type) {
+			case "error": return res.status(500).json({ error: reqResult.errors[0] })
+			case "success": return res.status(200).json({ data: reqResult.response })
+			default: throw "Unsplash не ответил на запрос"
+		}
+	} catch (e) {
+		return res.status(400).json({error: `ошибка: ${e.message}`})
+	}
 }
--- src/pages/api/randomPhotos.js
+++ src/pages/api/randomPhotos.js
@@ -2,10 +2,10 @@ import {unsplash} from '.'
 
 export default async function handler(req, res) {
 	try {
-		await unsplash.photos.getRandom({count: 30}).then(data => {
+		await unsplash.photos.getRandom({ count: 30 }).then(data => {
 			return res.status(200).json({data})
 		})
 	} catch {
-		res.status(500).json({data: 'Сервер даун'})
+		res.status(500).json({data: 'Сервер мёртв внутри'})
 	}
 }
