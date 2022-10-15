bf8ac0fd9328e253360d12e8b69b52cab479777f
Daniil Simanovich <s1mahochukvas@vk.com>
2022-10-15 11:46:19 +0300

    requests modified

--- src/funcs/apiRequest/pinGenerateQuery.js
+++ src/funcs/apiRequest/pinGenerateQuery.js
@@ -1,1 +1,7 @@
-export default async function () {}
+import axios from "axios"
+
+export default async function (params) {
+	const { id } = params
+	const res = (await axios.post(process.env.API_PATHS.pinGenerate, {id})).data
+	return res
+}
