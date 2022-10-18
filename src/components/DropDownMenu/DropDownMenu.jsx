ab3aec8e809f00072ca41eb70accdddd38b719be
Daniil Simanovich <s1mahochukvas@vk.com>
2022-10-18 10:50:38 +0300

    DropDownMenu modified

--- src/components/DropDownMenu/DropDownMenu.jsx
+++ src/components/DropDownMenu/DropDownMenu.jsx
@@ -3,7 +3,7 @@ import styles from './DropDownMenu.module.scss'
 import {DropDownMenuIcon} from '../imgs'
 import {useOutside} from '../../hooks'
 
-const DropDownMenu = ({children, title, soLeft, NotPinterestLogo}) => {
+const DropDownMenu = ({children, title, leftmost, params}) => {
 	const [activityState, setActivityState] = useOutside([
 		`[data-menu-filter=${title}]`,
 	])
@@ -15,27 +15,40 @@ const DropDownMenu = ({children, title, soLeft, NotPinterestLogo}) => {
 			})}
 		</>
 	)
-	const Icon = () =>
-		NotPinterestLogo ? (
-			<NotPinterestLogo className={styles.main_icon} />
-		) : null
-	return (
-		<div className={styles.menu} data-menu-filter={title}>
-			<button
-				onClick={() => setActivityState(!activityState)}
-				className={`${styles.menu__button} ${
-					activityState ? styles.menu__button_active : ''
-				}`}>
-				<Icon />
+	const BtnContent = () => {
+		if (!params) return (
+			<>
 				{title}
 				<div className={styles.icon_wrapper}>
 					<DropDownMenuIcon className={styles.icon} />
 				</div>
+			</>	
+		)
+		if (params.type === 'sircle') {
+			return (
+				<params.icon />
+			)
+		} 
+	
+	}
+	return (
+		<div className={styles.menu} data-menu-filter={title}>
+			<button
+				onClick={() => setActivityState(!activityState)}
+				className={`
+				${styles.menu__button}
+				${params && params.type === 'sircle' ? styles.menu__button_sircle: ''}
+				${activityState ? styles.menu__button_active : ''}
+				`}>
+				<BtnContent />
 			</button>
 			<div
-				className={`${styles.links_wrapper} ${
-					activityState ? styles.links_wrapper_active : ''
-				} ${soLeft ? styles.soLeft : ''}`}>
+				className={`
+				${styles.links_wrapper}
+				${params && params.type === 'sircle' ? styles.links_wrapper_sircle : ''} 
+				${activityState ? styles.links_wrapper_active : ''} 
+				${leftmost ? styles.leftmost : ''}
+				`}>
 				<ul
 					className={styles.links}
 					onClick={e =>
