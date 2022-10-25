8327815708764d46e52d50fad78a183478b4411b
Daniil Simanovich <s1mahochukvas@vk.com>
2022-10-25 21:58:31 +0300

    Btn component modified

--- src/components/Btn/Btn.jsx
+++ src/components/Btn/Btn.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import styles from './Btn.module.scss'
 
-const Btn = ({children}) => {
-	return <button className={styles.button}>{children}</button>
+const Btn = ({children, onClick, type, className}) => {
+	return <button onClick={onClick} className={`
+		${styles.button} 
+		${className}
+		${styles["button_" + type] || ''}
+	`}>{children}</button>
 }
 
 export default Btn
