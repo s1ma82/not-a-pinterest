cb1629d6fec5b87c1d4107f5eb4ceba87989e53d
Daniil Simanovich <s1mahochukvas@vk.com>
2022-10-25 11:45:42 +0300

    pin save

--- src/pages/pin/[id].js
+++ src/pages/pin/[id].js
@@ -1,17 +1,39 @@
 import React from 'react';
 import pinGenerateQuery from '../../helpers/apiRequest/pinGenerateQuery';
-import {DropDownMenu, ImgComp} from '../../components'
+import {DropDownMenu, ImgComp, ShareLink} from '../../components'
 import styles from './pin.module.scss'
 import { generateImgParams } from '../../helpers';
+import { ShareIcon, ThreeDot } from '../../components/imgs';
 const Pin = ({ data }) => {
-
+    const shareLinksParams = {
+        tg: {
+            name: "Telegram",
+            type: "sircle",
+            url: "google.com",
+            text: "gooloogooloo"
+        },
+        vk: {
+            name: "Vk",
+            type: "sircle",
+            url: 'vk.com',
+            text: "vk"
+        }
+    }
     return (
         <div className={styles.page}>
             <div className={styles.pin} style={{height: data.params.height}}>
                 <ImgComp data={data} type="pin" />
                 <div className={styles.info}>
-                    <DropDownMenu btnStyle="sircle" menuType="share" title="Share to" icon={} >
-                        <a href="/">ссылка</a>
+                    <DropDownMenu btnStyle="sircle" title="Helps" Icon={ThreeDot}>
+                        <a href = "/" >Какая-то ссылка</a>
+                        <a href = "/" >Какая-то ссылка</a>
+                        <a href = "/" >Какая-то ссылка</a>
+                    </DropDownMenu>
+                    <DropDownMenu btnStyle="sircle" menuType="share" title="Share to" Icon={ShareIcon} >
+                        <ShareLink params = {shareLinksParams.tg}/>
+                        <ShareLink params = {shareLinksParams.vk}/>
+                        <ShareLink params = {shareLinksParams.tg}/>
+                        <ShareLink params = {shareLinksParams.vk}/>
                     </DropDownMenu>
                 </div>
             </div>    
