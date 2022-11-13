2622444f4758e45c813e54a0aaad711296fc7e95
Daniil Simanovich <s1mahochukvas@vk.com>
2022-11-13 11:42:34 +0300

    flex

--- src/pages/pin/[id].js
+++ src/pages/pin/[id].js
@@ -38,44 +38,48 @@ const Pin = ({ data }) => {
         dispatch(modalAction({text: 'Ссылка скопирована в буфер обмена'}))
         // navigator.clipboard.writeText(JSON.stringify(user))
     }
-    return <div className={styles.page}>
-        <div className={styles.pin} style={{height: data.params.height}}>
-            <ImgComp data={data} type="pin" />
-            <div className={styles.info}>
-                <div className={styles.info__header}>
-                    <div className={styles.share_container}>
-                        <DropDownMenu btnStyle="sircle" title="Helps" Icon={ThreeDot}>
-                            <a href = "/" >Какая-то ссылка</a>
-                            <a href = "/" >Какая-то ссылка</a>
-                            <a href = "/" >Какая-то ссылка</a>
-                        </DropDownMenu>
-                        <DropDownMenu btnStyle="sircle" menuType="share" title="Share to" Icon={ShareIcon} >
-                            <ShareLink params = {shareLinksParams.tg}/>
-                            <ShareLink params = {shareLinksParams.vk}/>
-                        </DropDownMenu>
-                        <Btn type="sircle" onClick={copyUrl}><PathCopyIcon/></Btn>
+    return (
+        <div className={styles.page}>
+            <div className={styles.pin}>
+                <div className={styles.img_container}>
+                    <ImgComp data={data} type="pin" />
+                </div>
+                <div className={styles.info}>
+                    <div className={styles.info__header}>
+                        <div className={styles.share_container}>
+                            <DropDownMenu btnStyle="sircle" title="Helps" Icon={ThreeDot}>
+                                <a href = "/" >Какая-то ссылка</a>
+                                <a href = "/" >Какая-то ссылка</a>
+                                <a href = "/" >Какая-то ссылка</a>
+                            </DropDownMenu>
+                            <DropDownMenu btnStyle="sircle" menuType="share" title="Share to" Icon={ShareIcon} >
+                                <ShareLink params = {shareLinksParams.tg}/>
+                                <ShareLink params = {shareLinksParams.vk}/>
+                            </DropDownMenu>
+                            <Btn type="sircle" onClick={copyUrl}><PathCopyIcon/></Btn>
+                        </div>
+                        <div className={styles.accountSave_container}>
+                            <DropDownMenu btnStyle="default" title="Профиль" >
+                                <p>Какая то ересь, даже не ссылка</p> 
+                            </DropDownMenu>    
+                            <Btn className={styles.accountSave_btn}>Сохранить</Btn>
+                        </div>
                     </div>
-                    <div className={styles.accountSave_container}>
-                        <DropDownMenu btnStyle="default" title="Профиль" >
-                            <p>Какая то ересь, даже не ссылка</p> 
-                        </DropDownMenu>    
-                        <Btn className={styles.accountSave_btn}>Сохранить</Btn>
+                    <div className={styles.profile_container}>
+                        <div className={styles.byDownload}>
+                            <span className={styles.title}>Загружено:</span>
+                            <ProfileLink params={{
+                                name: profileParams.name,
+                                href: profileParams.href,
+                                type: "text"
+                            }} />
+                        </div>
+                        <ProfileLink className={styles.profile} params={profileParams} />
                     </div>
                 </div>
-                <div className={styles.profile_container}>
-                    <div className={styles.byDownload}>
-                        <span className={styles.title}>Загружено:</span>
-                        <ProfileLink params={{
-                            name: profileParams.name,
-                            href: profileParams.href,
-                            type: "text"
-                        }} />
-                    </div>
-                    <ProfileLink className={styles.profile} params={profileParams} />
-                </div>
-            </div>
-        </div>    
-    </div>
+            </div>    
+        </div>
+    )
 }
 
 export const getServerSideProps = async (context) => {
